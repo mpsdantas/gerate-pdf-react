729d0babc7f9b2be836c609d4dac221869195329
Marcus Dantas <mpsdantas15@gmail.com>
2018-07-18 00:55:07 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import html2canvas from 'html2canvas';
+import * as jsPDF from 'jspdf';
+
+class App extends Component {
+    printDocument() {
+        const input = document.getElementById('divToPrint');
+        html2canvas(input)
+            .then((canvas) => {
+                const imgData = canvas.toDataURL('image/png');
+                const pdf = new jsPDF();
+                pdf.addImage(imgData, 'JPEG', 0, 0);
+                // pdf.output('dataurlnewwindow');
+                pdf.save("download.pdf");
+            })
+            ;
+    }
+
+    render() {
+        return (<div>
+            <div className="mb5">
+                <button onClick={this.printDocument}>Salvar pdf</button>
+            </div>
+            <div id="divToPrint" className="mt4" style={{
+                backgroundColor: '#ffffff',
+                width: '210mm',
+                minHeight: '297mm',
+                marginLeft: 'auto',
+                marginRight: 'auto'
+            }}>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>);
+    }
+}
+export default App;
